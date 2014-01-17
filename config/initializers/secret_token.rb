65225d15bdf0eedf7fc1051cc8c6e422e7ed3480
Arash <arash.ouji@safe.com>
2014-01-17 11:30:05 -0800

    create controller and model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AwesomeAnswers::Application.config.secret_key_base = 'ca09e5ef4552fea00dc7b018ed3074c0ea8799e65535711b2c5ce9019f1a27e8d82b9277733b26ab76437b942994df8a674bc20ba71457003dbf7ac106a17deb'
